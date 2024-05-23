6cab37fe972061143ead7e51b88cddc48f4b00d6
CodeWithMilap <milapdave15@gmail.com>
2024-05-23 20:29:19 +0530

    Update MenuLink.js

--- components/Header/MenuLink.js
+++ components/Header/MenuLink.js
@@ -2,7 +2,7 @@ import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
 const MenuLink = ({ blok }) => (
   <Link
-    href={blok.link.cached_url}
+    href={`/${blok.link.cached_url}`}
     {...storyblokEditable(blok)}
     className="relative block px-3 py-2 transition hover:text-blue-600 dark:hover:text-blue-700"
   >
