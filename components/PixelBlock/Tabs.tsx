2748d9f1ad57f05d91a2f9ab343aaa724ad1f4cb
CodeWithMilap <milapdave15@gmail.com>
2024-06-12 09:20:32 +0530

    feat: add radius props

--- components/PixelBlock/Tabs.tsx
+++ components/PixelBlock/Tabs.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function Tabs({ defaultValue, className, variant, children }: any) {
+function Tabs({ defaultValue, className, variant, children , radius }: any) {
   const [activeTab, setActiveTab] = useState(defaultValue);
 
   return (
@@ -8,7 +8,7 @@ function Tabs({ defaultValue, className, variant, children }: any) {
       <div className="flex flex-col gap-5">
         {React.Children.map(children, (child) => {
           if (child.type === TabsList) {
-            return React.cloneElement(child, { activeTab, setActiveTab, variant });
+            return React.cloneElement(child, { activeTab, setActiveTab, variant , radius});
           } else if (child.type === TabsContent && child.props.value === activeTab) {
             return child;
           }
@@ -19,12 +19,12 @@ function Tabs({ defaultValue, className, variant, children }: any) {
   );
 }
 
-function TabsList({ children, className, activeTab, setActiveTab, variant = 'underline' }: any) {
+function TabsList({ children, className, activeTab, setActiveTab, variant = 'underline' , radius  }: any) {
 
   const styleClass = (() => {
     switch (variant) {
       case 'solid':
-        return 'inline-flex h-10 items-center justify-center bg-gray-100 p-1 text-gray-500 dark:bg-gray-800 dark:text-gray-400 rounded-md';
+        return 'inline-flex h-10 items-center justify-center bg-gray-100 p-1 text-gray-500 dark:bg-gray-800 dark:text-gray-400 ';
       case 'underline':
         return 'flex gap-2';
       default:
@@ -32,25 +32,39 @@ function TabsList({ children, className, activeTab, setActiveTab, variant = 'und
     }
   })();
 
+  const borderRadiusStyle = (() => {
+    switch (radius) {
+      case 'full':
+        return 'rounded-full';
+      case 'lg':
+        return 'rounded-lg';
+      case 'md':
+        return 'rounded-md';
+      case 'sm':
+        return 'rounded-sm';
+      default:
+        return 'rounded-sm';
+    }
+  })();
 
   return (
     <div className={`${className}`} role="tablist">
-      <div className={` ${styleClass}`}>
+      <div className={` ${styleClass} ${borderRadiusStyle}`}>
         {React.Children.map(children, (child) => {
-          return React.cloneElement(child, { activeTab, setActiveTab, variant });
+          return React.cloneElement(child, { activeTab, setActiveTab, variant , radius});
         })}
       </div>
     </div>
   );
 }
 
-function TabsTrigger({ value, children, className, activeTab, setActiveTab, variant = 'underline' }: any) {
+function TabsTrigger({ value, children, className, activeTab, setActiveTab, variant = 'underline' , radius }: any) {
   const isActive = activeTab === value;
 
   const styleClass = (() => {
     switch (variant) {
       case 'solid':
-        return 'inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-950 data-[state=active]:shadow-sm dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300 dark:data-[state=active]:bg-gray-950 dark:data-[state=active]:text-gray-50 rounded-md';
+        return 'inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-950 data-[state=active]:shadow-sm dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300 dark:data-[state=active]:bg-gray-950 dark:data-[state=active]:text-gray-50';
       case 'underline':
         return `inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-b-2 border-transparent data-[state=active]:border-black dark:data-[state=active]:text-gray-50 dark:data-[state=active]:border-white data-[state=active]:text-gray-950 '}`;
       default:
@@ -58,9 +72,24 @@ function TabsTrigger({ value, children, className, activeTab, setActiveTab, vari
     }
   })();
 
+  const borderRadiusStyle = (() => {
+    switch (radius) {
+      case 'full':
+        return 'rounded-full';
+      case 'lg':
+        return 'rounded-lg';
+      case 'md':
+        return 'rounded-md';
+      case 'sm':
+        return 'rounded-sm';
+      default:
+        return 'rounded-sm';
+    }
+  })();
+
   return (
     <button
-      className={`${className} ${styleClass}`}
+      className={`${className} ${styleClass} ${borderRadiusStyle}`}
       onClick={() => setActiveTab(value)} data-state={isActive ? "active" : "inactive"}
       role="tab"
       aria-selected={isActive ? "true" : "false"}
