588a2791a09154f31b3b03ca575c14e5c1bb3bd8
CodeWithMilap <milapdave15@gmail.com>
2024-07-28 16:17:29 +0530

    Update Dropdown.tsx

--- components/PixelBlock/Dropdown.tsx
+++ components/PixelBlock/Dropdown.tsx
@@ -7,7 +7,7 @@ interface DropdownProps {
   placeholder: string;
   onSelect: (selected: string[]) => void;
   multiple?: boolean;
-  renderOption?: (option: { value: string; label: string; disabled?: boolean }) => React.ReactNode;
+  renderOption?: (option: { value: string; label: string; disabled?: boolean, isSelected: boolean }) => React.ReactNode;
   searchPlaceholder?: string;
   selectedOptions?: string[];
   isOpen?: boolean;
@@ -92,11 +92,16 @@ const Dropdown: React.FC<DropdownProps> = ({
     };
   }, []);
 
-  const renderOptionDefault = (option: { value: string; label: string; disabled?: boolean }) => (
-    <div className={`py-2 text-sm ${option.disabled ? 'text-gray-400 cursor-not-allowed' : 'cursor-pointer'}`}>
-      {option.label}
-    </div>
-  );
+  const renderOptionDefault = (option: { value: string; label: string; disabled?: boolean }) => {
+    const isSelected = selected.includes(option.value);
+    return (
+      <div
+        className={`py-2 text-sm hover:bg-blue-50 hover:text-inherit hover:dark:bg-slate-900 px-3  ${isSelected ? 'bg-blue-500 text-white' : ''} ${option.disabled ? 'text-gray-400 cursor-not-allowed' : 'cursor-pointer'}`}
+      >
+        {option.label}
+      </div>
+    );
+  };
 
   return (
     <div className="relative w-64 border border-gray-300 rounded-md overflow-hidden" ref={dropdownRef}>
@@ -117,29 +122,37 @@ const Dropdown: React.FC<DropdownProps> = ({
       </button>
 
       {isOpen && (
-        <div className="z-10 border-t border-gray-300  bg-white w-full  dark:bg-zinc-700 overflow-hidden">
+        <div className="z-10 border-t border-gray-300 bg-white w-full dark:bg-zinc-700 overflow-hidden">
           {withSearch && (
-            <div className='p-3'>
+            <div className="p-3">
               <label htmlFor="input-group-search" className="sr-only">Search</label>
               <div className="relative">
                 <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                  <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                   </svg>
                 </div>
-                <input type="text" id="input-group-search"  value={searchTerm} onChange={handleSearch} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={searchPlaceholder} />
+                <input
+                  type="text"
+                  id="input-group-search"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder={searchPlaceholder}
+                />
               </div>
             </div>
-
           )}
           <div className="max-h-60 overflow-y-auto">
             {filteredOptions.map((option, index) => (
               <div
                 key={index}
-                className={` hover:bg-blue-50 hover:dark:bg-slate-900 px-3 ${option.disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                className={`${option.disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
                 onClick={() => !option.disabled && handleSelect(option.value)}
               >
-                {renderOption ? renderOption(option) : renderOptionDefault(option)}
+                {renderOption
+                  ? renderOption({ ...option, isSelected: selected.includes(option.value) })
+                  : renderOptionDefault(option)}
               </div>
             ))}
           </div>
