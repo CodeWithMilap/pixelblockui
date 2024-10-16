7119ce10a80115ee95f52277905bda20584f490f
CodeWithMilap <milapdave15@gmail.com>
2024-10-16 08:16:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    compress: true,
+    output: 'export',
+    images: {
+      formats: ['image/avif', 'image/webp'],
+    },
 };
 
 export default nextConfig;
